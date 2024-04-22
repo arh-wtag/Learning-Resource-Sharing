51b9e397389a960a391d27f88a3b43faa485b64c
Md Arafat Hossain <arafat.hossain@welldev.io>
2024-04-22 18:13:47 +0600

    dockerize my application

--- config/db.py
+++ config/db.py
@@ -1,11 +1,12 @@
 import psycopg2
 import os
+# print("+++++++",os.environ['DB_HOST'])
 connection = psycopg2.connect(
-    dbname = os.environ.get('DATABASE_NAME'),
-    user = os.environ.get('DATABASE_USER'),
-    password = os.environ.get('DATABASE_PASSWORD'),
-    host = 'localhost',
-    port = '5432'
+    host = os.environ.get('DB_HOST'),
+    port = os.environ.get('DB_PORT'),
+    dbname = os.environ.get('DB_NAME'),
+    user = os.environ.get('DB_USER'),
+    password = os.environ.get('DB_PASSWORD')
 )
 
 if(connection):
