d9022cca46f443351add530534ea68d4689e6b80
Md Arafat Hossain <arafat.hossain@welldev.io>
2024-05-19 17:46:38 +0600

    update: python_exporter

--- main.py
+++ main.py
@@ -2,20 +2,22 @@ from fastapi import FastAPI, Response, Request
 from routes.regestration.index import user
 from routes.login.index import log
 from routes.resources.index import res
-from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST, start_http_server
 from starlette.middleware import Middleware
 from starlette_exporter import PrometheusMiddleware
-
+import http.server
 app = FastAPI()
 
 app.include_router(user)
 app.include_router(log)
 app.include_router(res)
-REQUESTS_COUNT = Counter('app_r')
+REQUESTS_COUNT = Counter('app_requests_total', 'Total App Request')
 REQUESTS = Counter('http_requests_total', 'Total HTTP Requests')
 app.add_middleware(PrometheusMiddleware)
 @app.get("/")
 async def root():
+    METRICS_PORT=8001
+    start_http_server(METRICS_PORT)
     REQUESTS_COUNT.inc()
     REQUESTS.inc()
     return {"message": "Hello World"}
